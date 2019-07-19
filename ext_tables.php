f00d6d704c30d0244b0386a33bfcb9e6d302d843
David Imboden <david.imboden@iteration.info>
2019-07-19 18:51:36 +0200

    = 1.2.0 = * ADDED : Support for right to left languages. * FIXED : A bug displaying the bookable item's location and description twice. * IMPROVED : registration form layout.

--- ext_tables.php
+++ ext_tables.php
@@ -4,7 +4,6 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function()
     {
-
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
             'Sagenda.SagendaCalendarTypo3',
             'Sagendacalendartypo3',
@@ -13,11 +12,8 @@ call_user_func(
 
         $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['sagendacalendartypo3_sagendacalendartypo3'] = 'pi_flexform';
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('sagendacalendartypo3_sagendacalendartypo3', 'FILE:EXT:sagenda_calendar_typo3/Configuration/FlexForm/FlexForm.xml');
-        
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('sagenda_calendar_typo3', 'Configuration/TypoScript', 'Sagenda Calendar Typo3');
-
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_sagendacalendartypo3_domain_model_sagendacalendartypo3', 'EXT:sagenda_calendar_typo3/Resources/Private/Language/locallang_csh_tx_sagendacalendar_domain_model_sagendacalendar.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_sagendacalendartypo3_domain_model_sagendacalendartypo3');
-
     }
 );
